b71d5be809885fa2857f0020b6826cd94a47e84f
AndersonBaezP <andersonbaez1990@gmail.com>
2024-01-19 08:54:35 -0500

    cambios

--- registro.php
+++ registro.php
@@ -4,7 +4,7 @@ $email=$_POST['email']
 $name=$_POST['nombre'];
 $phone=$_POST['telefono'];
 
-$consulta="INSERT INTO proyectofinal.usuarios (email,nombre,telefono) values('$email','$name','$phone')";
+$consulta="INSERT INTO usuarios (email,nombre,telefono) values('$email','$name','$phone')";
 $resultado=mysqli_query($conexion,$consulta);
 if($resultado){
 echo "registro exitoso";
