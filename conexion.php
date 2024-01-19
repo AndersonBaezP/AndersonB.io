f0ab3dd31eeb45dafa261d058852e885649ad170
AndersonBaezP <andersonbaez1990@gmail.com>
2024-01-19 03:52:27 -0500

    agregar

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_host='localhost';
+$db_usuario='root';
+$db_paswprd='';
+$db_nombre='proyectofinal';
+
+$conexion=mysqli_connect($db_host,$db_usuario,$db_pasword,$db_nombre)
+
+?>
---
+++ registro.php
@@ -0,0 +1,13 @@
+<?php
+include 'conexion.php';
+$email=$_POST['email'];
+$nombre=$_POST['nombre'];
+$telefono=$_POST['telefono'];
+$consulta="INSERT INTO datos (email,nombre,telefono)values('$email','$nombre','$telefono')";
+$resultado=mysqli_query($conexion,$consulta);
+if($resultado){
+echo "registro exitoso";
+}else{
+    echo "no se pudo registrar";
+}
+?>
