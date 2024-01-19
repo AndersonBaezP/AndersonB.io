e2d4d88760d26f14cc341a1d712559da4366c3fc
AndersonBaezP <andersonbaez1990@gmail.com>
2024-01-19 08:39:35 -0500

    cambios

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
 $db_host='localhost';
 $db_usuario='root';
-$db_paswprd='';
+$db_paswprd='Nijash2012.';
 $db_nombre='proyectofinal';
 
 $conexion=mysqli_connect($db_host,$db_usuario,$db_pasword,$db_nombre)
